b5e105bb70bd2572dc17a5913e76aebc528305e4
Courtney Cash <cashcourtney97@gmail.com>
2021-08-11 12:20:56 -0400

    commit tree b2484711623c9fb164fa367cffa86fe7236c370b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+function superbowlWin(record) {
+    const winningSeason = record.find(record => record.result === "W") 
+    return !!winningSeason ? winningSeason.year : undefined;
+}
 
+
+ // find is going to iterate through each object in record. it is going to look at the results in each of the objects. it is going to check to see if the value of record.result is equal to this string ("W")
+
+ // "!" bang operator inverts the truth value 
+ // "!!" two bang operators 
+ // "?" short hand for if 
+ 
